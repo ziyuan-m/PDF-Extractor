f64ad64be8b3457cfcaf7ff0635374622e2d2bbe
ziyuan-m <52099476+ziyuan-m@users.noreply.github.com>
2019-06-29 13:41:25 +0800

    Delete PdfToken.java

--- src/PdfToken.java
+++
@@ -1,9 +0,0 @@
-public class PdfToken {
-    public String type;
-    public String lexeme;
-
-    public PdfToken(String type, String lexeme) {
-        this.type = type;
-        this.lexeme = lexeme;
-    }
-}
